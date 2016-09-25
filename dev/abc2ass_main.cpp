35238fc5d28a366ac16434366f1fd09ea02a1f77
Nicholas Yue <yue.nicholas@gmail.com>
2016-09-25 11:40:54 -0700

    Tidying up the format of the template method interpolate

--- dev/abc2ass_main.cpp
+++ dev/abc2ass_main.cpp
@@ -25,19 +25,19 @@ template <typename T> void interpolate(const Imath::Vec3<T>& P1,
 									   const Imath::Vec3<T>& T1,
 									   const Imath::Vec3<T>& P2,
 									   const Imath::Vec3<T>& T2,
-									   T s,
-									   Imath::Vec3<T>& P)
+									   T                     s,
+									   Imath::Vec3<T>&       P)
 {
-	T h1 =  2.0*std::pow(s,3.0) - 3.0*std::pow(s,2.0) + 1.0;          // calculate basis function 1
-	T h2 = -2.0*std::pow(s,3.0) + 3.0*std::pow(s,2.0);              // calculate basis function 2
-	T h3 =   std::pow(s,3.0) - 2.0*std::pow(s,2.0) + s;         // calculate basis function 3
-	T h4 =   std::pow(s,3.0) -  std::pow(s,2.0);              // calculate basis function 4
+	T h1 =  2.0*std::pow(s,3.0) - 3.0*std::pow(s,2.0) + 1.0; // calculate basis function 1
+	T h2 = -2.0*std::pow(s,3.0) + 3.0*std::pow(s,2.0);       // calculate basis function 2
+	T h3 =      std::pow(s,3.0) - 2.0*std::pow(s,2.0) + s;   // calculate basis function 3
+	T h4 =      std::pow(s,3.0) -     std::pow(s,2.0);       // calculate basis function 4
 
 
-	P = h1*P1 +                    // multiply and sum all funtions
-			h2*P2 +                    // together to build the interpolated
-			h3*T1 +                    // point along the curve.
-			h4*T2;
+	P = h1*P1 + // multiply and sum all funtions
+		h2*P2 + // together to build the interpolated
+		h3*T1 + // point along the curve.
+		h4*T2;
 }
 
 typedef boost::multi_array<Imath::V3f, 2> V3fSamplingArray2D;
