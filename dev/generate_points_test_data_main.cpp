d2c4c794f3291b7d2c030b634fcd85a5c191befe
Nicholas Yue <yue.nicholas@gmail.com>
2016-09-25 17:37:57 -0700

    Added utility to generate test data

---
+++ dev/generate_points_test_data_main.cpp
@@ -0,0 +1,27 @@
+#include <ai.h>
+
+#include <Alembic/Util/All.h>
+#include <Alembic/AbcCoreAbstract/All.h>
+#include <Alembic/AbcCoreOgawa/All.h>
+#include <Alembic/AbcCoreHDF5/All.h>
+#include <Alembic/Abc/All.h>
+#include <Alembic/AbcCoreFactory/All.h>
+#include <Alembic/AbcGeom/All.h>
+#include <Alembic/AbcCollection/All.h>
+#include <Alembic/AbcMaterial/All.h>
+
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+#include <boost/format.hpp>
+#include <boost/multi_array.hpp>
+
+#include <OpenEXR/ImathVec.h>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
