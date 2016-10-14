abd71cc033284c67260bb6823a55b17202f423e2
Nicholas Yue <yue.nicholas@gmail.com>
2016-10-14 16:31:33 -0700

    Adding MD5 digest to verify alembic source file is same for generated velocity file

--- dev/velocities_sidecar/generate_velocities_sidecar_main.cpp
+++ dev/velocities_sidecar/generate_velocities_sidecar_main.cpp
@@ -15,9 +15,48 @@
 #include <boost/program_options.hpp>
 #include <OpenEXR/ImathVec.h>
 
+#include <openssl/md5.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 #include "VelocitySideCar.h"
 
 namespace po = boost::program_options;
+
+// Get the size of the file by its file descriptor
+unsigned long get_size_by_fd(int fd)
+{
+    struct stat statbuf;
+    if(fstat(fd, &statbuf) < 0) exit(-1);
+    return statbuf.st_size;
+}
+
+void get_md5(const std::string& i_filename, std::string& o_md5)
+{
+	unsigned char result[MD5_DIGEST_LENGTH];
+
+    int file_descript;
+    unsigned long file_size;
+    char* file_buffer;
+
+    file_descript = open(i_filename.c_str(), O_RDONLY);
+    if(file_descript < 0) exit(-1);
+
+    file_size = get_size_by_fd(file_descript);
+    printf("file size:\t%lu\n", file_size);
+
+    file_buffer = (char *)mmap(0, file_size, PROT_READ, MAP_SHARED, file_descript, 0);
+    MD5((unsigned char*) file_buffer, file_size, result);
+    munmap(file_buffer, file_size);
+
+    o_md5 = reinterpret_cast<char*>(result);
+}
 
 int main(int argc, char** argv)
 {
@@ -37,16 +76,15 @@ int main(int argc, char** argv)
 		po::store(po::parse_command_line(argc, argv, desc), vm);
 		po::notify(vm);
 
-		if (vm.count("help")) {
+		if (vm.count("help") || alembic_file.empty() || velocity_file.empty()) {
 			std::cout << desc << "\n";
 			return 1;
 		}
-		if (vm.count("abc")) {
-			std::cout << "alembic_file variable " << alembic_file.c_str() << std::endl;
-		}
-		if (vm.count("vsc")) {
-			std::cout << "velocity_file variable " << velocity_file.c_str() << std::endl;
-		}
+
+		std::string md5;
+		get_md5(alembic_file, md5);
+		std::cout << boost::format("md5 = '%1%'") % md5 << std::endl;
+
 	}
 	catch(std::exception& e) {
 		std::cerr << "error: " << e.what() << "\n";
